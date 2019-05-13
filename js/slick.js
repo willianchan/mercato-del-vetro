068df915cd3d634073db343714928a913d15b7fc
Chan <willian_chan_@hotmail.com>
2019-05-13 03:14:56 -0300

    commit inicial com novo template

---
+++ js/slick.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".slider").slick({
+        arrows: true,
+        dots: true,
+        infinite: true,
+        centerMode: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: true
+    });
+});
