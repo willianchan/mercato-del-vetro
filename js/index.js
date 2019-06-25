dd3d986065684c7c663b9468d18871a3fae04995
unknown <marcelloscat@hotmail.com>
2019-06-25 16:26:10 -0300

    b

--- js/index.js
+++ js/index.js
@@ -16,11 +16,44 @@
       }
     }
   });
-
+  $('.modal').on('shown.bs.modal', function (e) {
+    $('.img-slider').resize();
+});
   // Closes responsive menu when a scroll trigger link is clicked
   $('.js-scroll-trigger').click(function () {
     $('.navbar-collapse').collapse('hide');
   });
+
+  $('body').on('hidden.bs.modal', function () {
+    if($('.modal.in').length > 0)
+    {
+        $('body').addClass('modal-open');
+    }
+});
+$(document).ready(function () {
+
+  $('.modal').on("hidden.bs.modal", function (e) { //fire on closing modal box
+         if ($('.modal:visible').length) { // check whether parent modal is opend after child modal close
+             $('body').addClass('modal-open'); // if open mean length is 1 then add a bootstrap css class to body of the page
+         }
+     });
+ });
+ $(document).ready(function () {
+
+
+      $(document).on('show.bs.modal', '.modal', function (event) {
+          var zIndex = 1040 + (10 * $('.modal:visible').length);
+          $(this).css('z-index', zIndex);
+          setTimeout(function() {
+              $('.modal-backdrop').not('.modal-stack').css('z-index', zIndex - 1).addClass('modal-stack');
+          }, 0);
+      });
+
+
+});
+ $('.img-slider').slick('setPosition');
+ //this code segment will activate parent modal dialog 
+ //after child modal box close then scroll problem will automatically fixed
 
   // Activate scrollspy to add active class to navbar items on scroll
   $('body').scrollspy({
@@ -30,7 +63,7 @@
 
   // Collapse Navbar
   var navbarCollapse = function () {
-    if ($(window).scrollTop() > 840) {
+    if ($(window).scrollTop() > 250) {
       if (!isNavBarShown) {
         $(".navbar").hide();
       }
@@ -50,6 +83,23 @@
       isNavBarShown = false;
     }
   };
+  var modalShrink = function(){
+    if($(window).width<768){
+      $("modal-dialog").addClass("modal-dialog-shrink");
+    }
+  };
+  $("#navbar-btn").click(function(){
+    var elemento = document.getElementById('mainNav');
+    if(!elemento.classList.contains("black-on-show")){
+      $("#mainNav").addClass("black-on-show");
+    }
+    else{
+      $("#mainNav").removeClass("black-on-show");
+      
+      
+      
+    }
+  });
   // Collapse now if page is not at top
   navbarCollapse();
   // Collapse the navbar when page is scrolled
