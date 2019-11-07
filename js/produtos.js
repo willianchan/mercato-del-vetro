18eb519ffa740fb42d8d821a7817ca128d50927d
unknown <marcelloscat@hotmail.com>
2019-11-07 16:36:19 -0300

    mais em frente

--- js/produtos.js
+++ js/produtos.js
@@ -1,8 +1,7 @@
-function getProd(theUrl)
-{
+function getProd(theUrl) {
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-    xmlHttp.send( null );
+    xmlHttp.open("GET", theUrl, false); // false for synchronous request
+    xmlHttp.send(null);
     return xmlHttp.responseText;
 }
 
@@ -11,23 +10,22 @@ function addProduto(produto) {
     div.className = 'col-md-4 col-sm-6 vidros-item';
 
     div.innerHTML = `
-        <a class="vidros-link" data-toggle="modal" href="#produtoModal`+produto.id+`">
+        <a class="vidros-link" data-toggle="modal" href="#produtoModal`+ produto.id + `">
             <div class="vidros-hover">
                 <div class="vidros-hover-content">
                     <i class="fas fa-search-plus fa-3x"></i>
                 </div>
             </div>
-            <img class="img-fluid" src="`+produto.imagem+`" alt="">
+            <img class="img-fluid" src="`+ produto.imagem + `" alt="">
         </a>
         <div class="vidros-caption">
-            <h4>`+produto.titulo+`</h4>
-            <a href="#expand`+produto.id+`" data-toggle="collapse" style="float: right; color: black; font-size:15px; margin-top:-25px"><i class="fas fa-chevron-down"></i></a>
+            <h4>`+ produto.titulo + `</h4>
+            <a href="#expand`+ produto.id + `" data-toggle="collapse" style="color: black; font-size:15px; margin-top:-25px"><i class="fas fa-chevron-down"></i></a>
 
-        <div id="expand`+produto.id+`" class="collapse" style="">
-            <div style="margin-top: 10px" class="row">
-                
-                <div class="col-md-6"><a onClick="editar(`+produto.id+`,'`+produto.titulo+`',`+`'`+produto.texto+`',`+`'`+produto.imagem+`'`+`)" class="btn btn-primary btn-sm textoBranco" style="cursor: pointer;"><i class="fas fa-edit"></i> Editar</a></div>
-                <div class="col-md-6"><a onClick="deletar(`+produto.id+`)" class="btn btn-primary btn-sm textoBranco" style="cursor: pointer; background-color:#d11a2a !important; border-color:#d11a2a !important"><i class="fas fa-trash-alt"></i> Deletar</a></div>
+            <div id="expand`+ produto.id + `" class="collapse" style="">
+                <div style="margin-top: 10px" class="row">
+                <div class="col-md-6"><a onClick="editar(`+ produto.id + `,'` + produto.titulo + `',` + `'` + produto.texto + `',` + `'` + produto.imagem + `'` + `)" class="btn btn-primary btn-sm textoBranco" style="cursor: pointer;"><i class="fas fa-edit"></i> Editar</a></div>
+                <div class="col-md-6"><a onClick="deletar(`+ produto.id + `)" class="btn btn-primary btn-sm textoBranco" style="cursor: pointer; background-color:#d11a2a !important; border-color:#d11a2a !important"><i class="fas fa-trash-alt"></i> Deletar</a></div>
             </div>
         </div>
         </div>
@@ -38,8 +36,8 @@ function addProduto(produto) {
 }
 function addModal(produto) {
     const div = document.createElement('div');
-    div.innerHTML =`
-    <div class="vidros-modal modal fade" id="produtoModal`+produto.id+`" tabindex="-1" role="dialog" aria-hidden="true">
+    div.innerHTML = `
+    <div class="vidros-modal modal fade" id="produtoModal`+ produto.id + `" tabindex="-1" role="dialog" aria-hidden="true">
         <div class="close-modal" data-dismiss="modal">
             <i class="fas fa-times externo"></i>
         </div>
@@ -47,9 +45,9 @@ function addModal(produto) {
             <div class="modal-content">
                 <div class="container">
                     <div class="modal-body modal-reduce-width">
-                        <h2 class="text-uppercase">`+produto.titulo+`</h2>
-                        <img class="img-fluid d-block mx-auto vidros-moldura" src="`+produto.imagem+`" alt="">
-                        <p>`+produto.texto+`</p>
+                        <h2 class="text-uppercase">`+ produto.titulo + `</h2>
+                        <img class="img-fluid d-block mx-auto vidros-moldura" src="`+ produto.imagem + `" alt="">
+                        <p>`+ produto.texto + `</p>
                         <button class="btn btn-primary" data-dismiss="modal" type="button">
                             <i class="fas fa-times"></i>
                             Fechar</button>
@@ -78,28 +76,37 @@ function addAddButton() {
 }
 
 function editar(idv, titulov, textov, imagemv) {
-    document.location.href = `editar_produto.html?id=`+idv+`&titulo=`+titulov+`&texto=`+textov+`&imagem=`+imagemv;
+    document.location.href = `editar_produto.html?id=` + idv + `&titulo=` + titulov + `&texto=` + textov + `&imagem=` + imagemv;
 }
-function deletar(id){
-    var http = new XMLHttpRequest();
-    var url = 'http://localhost:5000/produtos/'+id;
-    if (true) {
-        http.open('DELETE', url, true);
-        document.location.href = "produtos.html";
-        window.alert("Produto deletado com sucesso!");
+function deletar(id) {
+    var url = "http://localhost:5000/produtos/" + id;
+    var xhr = new XMLHttpRequest();
+    xhr.open("DELETE", url, true);
+    if (confirm("Após deletado o produto não poderá ser recuperado. Deseja mesmo deletar?")) {
+        txt = "You pressed OK!";
+        xhr.onload = function () {
+            var produtos = JSON.parse(xhr.responseText);
+            if (xhr.readyState == 4 && xhr.status == "200") {
+                console.table(produtos);
+                document.location.href = "produtos.html";
+                alert("Produto deletado com sucesso!")
+            } else {
+                console.error(produtos);
+            }
+        }
+        xhr.send(null);
     }
-    else window.alert("Preencha todos os campos para adicionar o produto");
-    http.send(null);
+    
 }
 
-window.onload = function() {
+window.onload = function () {
     var produtos = getProd('http://localhost:5000/produtos');
     produtos = JSON.parse(produtos);
     console.log(produtos);
-    for(produto in produtos){
+    for (produto in produtos) {
         console.log(produtos[produto]);
         this.addProduto(produtos[produto]);
-        this.addModal(produtos[produto]); 
+        this.addModal(produtos[produto]);
     }
     this.addAddButton();
-  };
+};
